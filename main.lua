3b6eb68232575bdb42fc9879cee697746eb59dc9
lukehayes <lukepurplehayes@gmail.com>
2024-10-08 09:59:23 +0100

    Added test map loop to main

--- main.lua
+++ main.lua
@@ -1,6 +1,23 @@
 local go  = require "game.GameObject"
+local Color  = require "game.gfx.Color"
 
 local obj = go(100,200)
+
+local img = love.image.newImageData("assets/map.png")
+
+local map = {}
+
+
+for x=1, img:getWidth() do
+
+    local row = {}
+    for y=1, img:getHeight() do
+        local c = Color(img:getPixel(x-1,y-1))
+        table.insert(row, c)
+    end
+
+    table.insert(map, row)
+end
 
 function love.load()
 end
@@ -24,6 +41,28 @@ function love.draw()
         obj.size.x,
         obj.size.y
     )
+
+    for x,row in pairs(map) do
+        for y,c in pairs(row) do
+            print(x,y,c)
+
+            --love.graphics.setcolor(
+                --c.r,
+                --c.g,
+                --c.b,
+                --c.a
+            --)
+
+            --love.graphics.rectangle(
+                --"fill",
+                --x * 16,
+                --y * 16,
+                --16,
+                --16
+            --)
+        end
+    end
+
 
 end
 
