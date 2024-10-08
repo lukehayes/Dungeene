0fc88e2b79772b6e160e737331f730bf0083d13b
lukehayes <lukepurplehayes@gmail.com>
2024-10-08 08:54:32 +0100

    Implemented GameObject:__tostring()

--- game/GameObject.lua
+++ game/GameObject.lua
@@ -1,4 +1,4 @@
-Object = require "libs.classic"
+Object   = require "libs.classic"
 local V2 = require "game.math.Vector2"
 
 GameObject = Object:extend()
@@ -10,5 +10,11 @@ function GameObject:new(x,y)
     self.color    = {r=0,g=1,b=0,a=1}
 end
 
+function GameObject:__tostring()
+    return "GameObject: \n" ..
+           "\tPosition: {" .. self.position.x .. "," .. self.position.y .."}\n" ..
+           "\tSize: {" .. self.size.x .. "," .. self.size.y .."}" ..
+           "\n"
+end
 
 return GameObject
