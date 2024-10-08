676e39e9bad2c1633472c299c0b6a95da029db0d
lukehayes <lukepurplehayes@gmail.com>
2024-10-08 09:59:13 +0100

    Added Color helper functions

--- game/gfx/Color.lua
+++ game/gfx/Color.lua
@@ -10,11 +10,45 @@ function Color:new(r,g,b,a)
     self.a = a or 1
 end
 
+
 function Color:__tostring()
     return "Color: {R=" .. self.r ..
             ", G=" .. self.g .. "" ..
             ", B=" .. self.b .. "" ..
             ", A=" .. self.a .. "}"
+end
+
+function Color.isWhite(color)
+
+    local isWhite =
+              color.r == 1 and
+              color.g == 1 and
+              color.b == 1 and
+              color.a == 1
+
+    return isWhite or false
+end
+
+function Color.isBlack(color)
+
+    local isBlack =
+              color.r == 0 and
+              color.g == 0 and
+              color.b == 0 and
+              color.a == 1
+
+    return isBlack or false
+end
+
+
+-- STATIC COLORS
+--
+function Color.WHITE()
+    return Color(1,1,1,1)
+end
+
+function Color.BLACK()
+    return Color(0,0,0,1)
 end
 
 return Color
