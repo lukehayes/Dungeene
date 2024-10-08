45d8a5b36f98149224ad6d9dc9987ee03ffba564
lukehayes <lukepurplehayes@gmail.com>
2024-10-08 09:03:29 +0100

    Added GameObject to main

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+local go  = require "game.GameObject"
+
+local obj = go(100,200)
+
 function love.load()
 end
 
@@ -5,6 +9,24 @@ function love.update(dt)
 end
 
 function love.draw()
+
+    love.graphics.setColor(
+        obj.color.r,
+        obj.color.g,
+        obj.color.b,
+        obj.color.a
+    )
+
+    love.graphics.rectangle(
+        "fill",
+        obj.position.x,
+        obj.position.y,
+        obj.size.x,
+        obj.size.y
+    )
+
+end
+
 function love.keypressed(key, scancode, isrepeat)
    if key == "escape" then
       love.event.quit()
