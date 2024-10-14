d0ea56e41b21ced9f2454f2d567ec26bf85b037e
lukehayes <lukepurplehayes@gmail.com>
2024-10-14 11:23:29 +0100

    Upated main

--- main.lua
+++ main.lua
@@ -1,36 +1,39 @@
 local go  = require "game.GameObject"
 local R  = require "game.gfx.Renderer"
 local Color  = require "game.gfx.Color"
-
-require "game.init"
-
+local AStar = require "libs.lua-star"
 
 local obj = go(100,200)
+local c = 0
+
+love.graphics.setDefaultFilter("nearest", "nearest")
 
 local img = love.image.newImageData("assets/map.png")
 local tiles = love.graphics.newImage("assets/tilesheet.png", {})
 
+local tileSize  = 16
+local tileScale = 2
+
+local floor = love.graphics.newQuad( 0, 0, tileSize, tileSize, tiles)
+local wall = love.graphics.newQuad( 16, 0, tileSize, tileSize, tiles)
+
 local map = {}
-
 for x=0, img:getWidth() -1 do
-
     local row = {}
     for y=0, img:getHeight() -1 do
         local c = Color(img:getPixel(x,y))
         table.insert(row, c)
     end
-
     table.insert(map, row)
 end
-
 function love.load()
 end
 
 function love.update(dt)
+    c = c + 0.1
 end
 
 function love.draw()
-
     R.clear()
 
     for x,row in pairs(map) do
@@ -45,16 +48,24 @@ function love.draw()
 
             if Color.isTransparent(c) ~= nil then
                 if Color.isWhite(c) then
-                    R.drawTile(tiles, x,y)
+                    local yPos = tileSize * tileScale
+                    love.graphics.draw(tiles, floor, x * tileSize , y * tileSize, 0, 1, 1)
+                     --R.drawTile(tiles, x,y)
                 end
             end
         end
     end
 
+    love.graphics.rectangle("fill", 200 + math.cos(c) * 100, 200 + math.sin(c) * 100, 32,32)
 end
 
 function love.keypressed(key, scancode, isrepeat)
-   if key == "escape" then
-      love.event.quit()
-   end
+  if key == "escape" then
+    love.event.quit()
+  end
+
+  if key == "space" then
+      print("Space")
+  end
 end
+
