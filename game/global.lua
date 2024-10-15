c5a83e8e3ad4ac43d8b6f39456cd24b97bd3d91e
lukehayes <lukepurplehayes@gmail.com>
2024-10-15 09:48:23 +0100

    Tiles are now generated in global.lua

--- game/global.lua
+++ game/global.lua
@@ -1,3 +1,7 @@
 TILE_SIZE  = 16
-TILE_SCALE = 2
+TILE_SCALE = 3
 
+love.graphics.setDefaultFilter("nearest", "nearest")
+tilesheet  = love.graphics.newImage("assets/tilesheet.png", {})
+tile_floor = love.graphics.newQuad( 0, 0, TILE_SIZE, TILE_SIZE, tilesheet)
+tile_wall  = love.graphics.newQuad( 16, 0, TILE_SIZE, TILE_SIZE, tilesheet)
