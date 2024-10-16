0d0d11f95a83ef4a549fd45dd9e8e8cce800d8b2
lukehayes <lukepurplehayes@gmail.com>
2024-10-16 09:40:51 +0100

    Changed to 2 space indentation

--- main.lua
+++ main.lua
@@ -15,17 +15,15 @@ for x=0, img:getWidth() -1 do
 
   for y=0, img:getHeight() -1 do
     local c = Color(img:getPixel(x,y))
-    row[y+2] = c
-    --table.insert(row, c)
+    table.insert(row, c)
   end
-  map[x+2] = row
-  --table.insert(map, row)
+  table.insert(map, row)
 end
 
 --for x,row in ipairs(map) do
-  --for y,c in ipairs(row) do
-    ----print(x,y)
-  --end
+--for y,c in ipairs(row) do
+----print(x,y)
+--end
 --end
 
 --print("------------")
@@ -38,69 +36,35 @@ function love.update(dt)
 end
 
 function love.draw()
+
   R.clear()
 
-  for x,row in ipairs(map) do
-    for y,c in ipairs(row) do
-      love.graphics.setColor(
-        c.r,
-        c.g,
-        c.b,
-        c.a
-      )
+  for x,row in pairs(map) do
+    for y,c in pairs(row) do
 
-      local scale = TILE_SIZE * TILE_SCALE
+        local xIsOne = (x == 1)
+        local xIsMax = (x == img:getWidth())
+        local yIsOne = (y == 1)
+        local yIsMax = (y == img:getHeight())
 
-      if Color.isWhite(c) then
-        local yPos = TILE_SIZE * TILE_SCALE
 
-        -- DRAW FLOOR
-        love.graphics.draw(tilesheet, tile_floor, x * scale , y * scale, 0, TILE_SCALE, TILE_SCALE)
-
-        -- DRAW SURROUNDING WALL TILES
-
-          if x >= 1 and x <= 12 then
-            print("X is:",x, map[x], map[x+1], map[x-1], "X -1 is:", x-1)
-          end
-
-        print(mapBoundsCheck(x,y,12,12))
-
-          NORTH = map[x][y-1]
-          SOUTH = map[x][y+1]
-          EAST = map[x+1][y]
+        if xIsOne then
+          WEST = map[1][y]
+        else
           WEST = map[x-1][y]
-          --local EAST = map[x+1][y]
-          --local WEST = map[x-1][y]
+        end
 
-        print(
-          NORTH,
-          SOUTH,
-          EAST,
-          WEST,
-          "END OF PRINT"
-        )
+        if xIsMax and not xIsOne then
+          WEST = map[x][y]
+        else
+          WEST = map[x-1][y]
+        end
 
-          if Color.isTransparent(NORTH) then
-            love.graphics.draw(tilesheet, tile_wall, x * scale , y * scale - scale, 0, TILE_SCALE, TILE_SCALE)
-          end
 
-          if Color.isTransparent(SOUTH) then
-            love.graphics.draw(tilesheet, tile_wall, x * scale , y * scale + scale, 0, TILE_SCALE, TILE_SCALE)
-          end
-
-          if Color.isTransparent(EAST) then
-            love.graphics.draw(tilesheet, tile_wall, x * scale + scale, y * scale, 0, TILE_SCALE, TILE_SCALE)
-          end
-
-          if Color.isTransparent(WEST) then
-            love.graphics.draw(tilesheet, tile_wall, x * scale - scale, y * scale, 0, TILE_SCALE, TILE_SCALE)
-          end
-
-        --R.drawTile(tiles, x,y)
-      end
 
     end
   end
+  print("--------------")
 
   love.graphics.rectangle("fill", 200 + math.cos(c) * 100, 200 + math.sin(c) * 100, 32,32)
 end
